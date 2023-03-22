dd455f656ac2dfbb267ba3f8911651f9ea7fc816
bbogomilov1 <bdbogomilov@gmail.com>
2023-03-22 12:01:58 +0200

    handling errors

--- 12-adv-starting-project/frontend/src/pages/ErrorPage.js
+++ 12-adv-starting-project/frontend/src/pages/ErrorPage.js
@@ -1,5 +1,30 @@
+import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+import PageContent from "../components/PageContent";
+
 const ErrorPage = () => {
-  return <h1>An error occurred</h1>;
+  const error = useRouteError();
+
+  let title = "An error occured";
+  let message = "Something went wrong";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found";
+    message = "Could not find page";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 };
 
 export default ErrorPage;

4b2d99172eebadb0a4c9918a644ea77392ac31c9
bbogomilov1 <bdbogomilov@gmail.com>
2023-04-04 15:22:09 +0300

    storing expiration token

--- events-app-master/12-adv-starting-project/frontend/src/pages/RootLayout.js
+++ events-app-master/12-adv-starting-project/frontend/src/pages/RootLayout.js
@@ -1,9 +1,4 @@
-import {
-  Outlet,
-  useLoaderData,
-  useNavigation,
-  useSubmit,
-} from "react-router-dom";
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 import { useEffect } from "react";
